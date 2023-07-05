85308e8b298437a46baaa72ae697a183e96e0eae
“Sairam-Nulakani” <sairamnulakani4@gmail.com>
2023-07-05 11:43:29 +0530

    handle form with multiple inputs

--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import "./formInput.css";
 
 const FormInput = (props) => {
+  const { label, onChange, id, ...inputProps } = props;
   return (
     <div className="formInput">
-      {/* <label>Username</label> */}
-      <input type="text" placeholder={props.placeholder} name={props.name} />
+      <label>{label}</label>
+      <input type="text" {...inputProps} onChange={onChange} />
     </div>
   );
 };
